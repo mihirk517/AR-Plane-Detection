36f88b424f97bc9d410dc6a40058d61d615f19d1
Mihir Kulkarni <50024720+mihirk517@users.noreply.github.com>
2021-10-20 15:03:12 +0200

    Add files via upload

---
+++ Classes/Native/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,69 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void ARTapToPlaceObject::Awake()
+extern void ARTapToPlaceObject_Awake_m0CEF4C47C1C398B9179F6032622014835181F6FF (void);
+// 0x00000002 System.Void ARTapToPlaceObject::Start()
+extern void ARTapToPlaceObject_Start_mCEA5A9ADC5ACD785F329254FD679C5513136EDD4 (void);
+// 0x00000003 System.Boolean ARTapToPlaceObject::TryGetTouchPosition(UnityEngine.Vector2&)
+extern void ARTapToPlaceObject_TryGetTouchPosition_m1730A440B93F1D82112B4A2C1E360986A48D66F6 (void);
+// 0x00000004 System.Void ARTapToPlaceObject::Update()
+extern void ARTapToPlaceObject_Update_mCEA015466DB37FE1DC8947F8B16E5773004E3F7C (void);
+// 0x00000005 System.Void ARTapToPlaceObject::.ctor()
+extern void ARTapToPlaceObject__ctor_mF5B2CD917913D8081CB42BA882632868D038267B (void);
+// 0x00000006 System.Void ARTapToPlaceObject::.cctor()
+extern void ARTapToPlaceObject__cctor_mBC951FA44087B2B924D7177416666CE4FC2FD5EA (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	ARTapToPlaceObject_Awake_m0CEF4C47C1C398B9179F6032622014835181F6FF,
+	ARTapToPlaceObject_Start_mCEA5A9ADC5ACD785F329254FD679C5513136EDD4,
+	ARTapToPlaceObject_TryGetTouchPosition_m1730A440B93F1D82112B4A2C1E360986A48D66F6,
+	ARTapToPlaceObject_Update_mCEA015466DB37FE1DC8947F8B16E5773004E3F7C,
+	ARTapToPlaceObject__ctor_mF5B2CD917913D8081CB42BA882632868D038267B,
+	ARTapToPlaceObject__cctor_mBC951FA44087B2B924D7177416666CE4FC2FD5EA,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	23,
+	23,
+	756,
+	23,
+	23,
+	3,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
